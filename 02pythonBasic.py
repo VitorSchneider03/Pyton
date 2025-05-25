7d66e51a52b2a37bae648bfb94a63123b60179dc
Vitor Schneider <flotita.editor@outlook.com>
2025-05-25 03:42:54 -0300

    Conhecimento basico python - variaveis, print e scan.

---
+++ 02pythonBasic.py
@@ -0,0 +1,14 @@
+#Variaveis em Python
+nome = 'Vitor'
+idade = 26
+peso = 67.5
+
+#Escrevendo na tela
+print('Seu nome é ' + nome + ', sua idade é', idade, 'e seu peso é', peso)
+
+#Recebendo valores
+nomerescebe = input('Qual é o seu nome? ')
+idaderescebe  = input('Qual a sua idade? ')
+pesorescebe = input('Qual seu peso? ')
+#'\n' Quebra linha
+print('Seu Nome: ', nomerescebe, '\nSua Idade: ', idaderescebe, '\nSeu Peso: ', pesorescebe)
