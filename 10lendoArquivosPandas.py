13bd52779b7a4ab858ba4203187dea61c767d9d6
Vitor Schneider <flotita.editor@outlook.com>
2025-05-28 18:51:42 -0300

    Manipulação dos dados

--- 10lendoArquivosPandas.py
+++ 10lendoArquivosPandas.py
@@ -3,7 +3,14 @@ import pandas as pd
 arq = pd.read_csv('Arquivos/tabela_ficticia.csv', sep=';')
 arq.columns = arq.columns.str.strip().str.replace('"', '')
 print(arq)
-print(arq.head())
-print(arq.tail())
-print(arq.columns)
-print(arq['NOME'])
+
+arq['DAQUI_5_ANOS'] = arq['IDADE'] + 5
+
+
+maior_de_30 = arq['IDADE'] > 30
+vendedores = arq['FUNCAO'] == 'Vendedor'
+maior_de_25 = arq['DAQUI_5_ANOS'] > 30
+print('\n\n Vendedores maiores de 30 anos:\n', arq[maior_de_30 & vendedores])
+print('\n\n Vendedores maiores de 30 em 5 anos:\n', arq[maior_de_25 & vendedores])
+nomes = arq.loc[maior_de_30 & vendedores, 'NOME'].tolist()
+print('\n\nVendedores maiores de 30 anos:\n', nomes)
