924d76b8bb5a5f5de69eb0799c1f82e81fe0ed6c
Vitor Schneider <flotita.editor@outlook.com>
2025-05-31 21:50:10 -0300

    Atividade tabuada.

--- 11tabuada.py
+++ 11tabuada.py
@@ -2,4 +2,4 @@ valor = int(input("Insira um valor inteiro para ver sua tabuada: "))
 for i in range(11):
     calc = valor * i
     print('{} x {} = {}'.format(i, valor, calc))
-    print('-' * 10)
+    print('-' * 9)
