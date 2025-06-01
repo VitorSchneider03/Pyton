1476402084e82ce5c7413c459f5140287d9e8958
Vitor Schneider <flotita.editor@outlook.com>
2025-06-01 20:20:50 -0300

    Manipulacao de texto - Divisao; Tamanho

---
+++ 16manipulacaoTexto2.py
@@ -0,0 +1,11 @@
+# Divisao
+frase = 'O Python é a cobra, o PHP é o Elefante'
+# .split é separar pelos espaços
+frase_split = frase.split()
+print(frase_split)
+print(frase_split[1])
+# .join é unir
+frase_unir = ' '.join(frase_split)
+print(frase_unir)
+# Tamanho da frase
+print(len(frase))
