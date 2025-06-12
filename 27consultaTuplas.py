185f65cbad3bdb60f5a721b6b378ee8e43cda157
Vitor Schneider <flotita.editor@outlook.com>
2025-06-11 23:37:51 -0300

    Consulta de tuplas

---
+++ 27consultaTuplas.py
@@ -0,0 +1,11 @@
+t1 = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+t2 = (4, 3, 2, 1, 3, 2)
+t3 = t1 + t2
+print(t3)
+print(sorted(t3))
+print(t3.count(4))
+print(t3.index(1))
+
+ddCadastrais = 'Vitor', 25, 'M', 67.5 #não precisa ser mesmo tipo
+print(ddCadastrais)
+del(ddCadastrais) #Deleta
